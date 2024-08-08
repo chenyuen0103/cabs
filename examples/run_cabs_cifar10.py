5d6dd95af08149714d5c926cf3a743dca7d75187
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-08-08 14:47:13 -0700

    add AddV2

--- examples/run_cabs_cifar10.py
+++ examples/run_cabs_cifar10.py
@@ -21,6 +21,7 @@ learning_rate = 0.1
 initial_batch_size = 16
 bs_min = 16
 bs_max = 2048
+eval_interval = 100
 ###############################################################################
 
 # Set up model
@@ -28,6 +29,14 @@ tf.reset_default_graph()
 global_bs = tf.Variable(tf.constant(initial_batch_size, dtype=tf.int32))
 images, labels = cifar10.inputs(eval_data=False, batch_size=global_bs)
 losses, variables = model.set_up_model(images, labels)
+
+
+# Test data
+test_images, test_labels, test_iterator = cifar10.inputs(eval_data=True, batch_size=global_bs)
+test_logits = model.build_model(test_images, variables, is_training=False)
+test_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=test_logits, labels=test_labels))
+test_accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(test_logits, axis=1), test_labels), tf.float32))
+
 
 # Set up CABS optimizer
 opt = CABSOptimizer(learning_rate, bs_min, bs_max)
@@ -39,11 +48,22 @@ coord = tf.train.Coordinator()
 sess.run(tf.global_variables_initializer())
 threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
+# # Run CABS
+# for i in range(num_steps):
+#   _, m_new, l = sess.run([sgd_step, bs_new, loss])
+#   print(l)
+#   print(m_new)
+
+
 # Run CABS
 for i in range(num_steps):
-  _, m_new, l = sess.run([sgd_step, bs_new, loss])
-  print(l)
-  print(m_new)
+  _, m_new, l = sess.run([sgd_step, bs_new, train_loss])
+  print(f'Step: {i + 1}, Train Loss: {l:.4f}, Batch Size: {m_new}')
+
+  if (i + 1) % eval_interval == 0:
+    sess.run(test_iterator.initializer)
+    test_acc, test_l = sess.run([test_accuracy, test_loss])
+    print(f'Step: {i + 1}, Test Accuracy: {test_acc:.4f}, Test Loss: {test_l:.4f}')
 
 # Stop queues
 coord.request_stop()
