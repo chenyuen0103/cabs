4b893e61a2c5f4e2319e095e0e7c514c36727429
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-08-10 15:28:36 -0700

    last try tf 1.15

--- examples/run_our_cifar10.py
+++ examples/run_our_cifar10.py
@@ -96,7 +96,7 @@ sess = tf.Session()
 coord = tf.train.Coordinator()
 sess.run(tf.global_variables_initializer())
 # Initialize the iterators before using them
-# sess.run(train_iterator.initializer)
+sess.run(train_iterator.initializer)
 # sess.run(val_iterator.initializer)
 threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
