ba5d2d80a6c83c6865d3e3da89870475435e8614
chenyuen0103 <chenyuen0103@berkeley.edu>
2024-08-09 23:07:05 -0700

    add __pycache__ to ignore

--- examples/cifar10_adaptive_batchsize.py
+++ examples/cifar10_adaptive_batchsize.py
@@ -262,6 +262,6 @@ def inputs(eval_data, data_dir=DATA_DIR, batch_size=128, indices=None):
     # Generate a batch of images and labels by building up a queue of examples.
     return _generate_image_and_label_batch(float_image, read_input.label,
                                          min_queue_examples, batch_size,
-                                         shuffle=False)
+                                         shuffle=False, max_batch_size=2048)
 
 
